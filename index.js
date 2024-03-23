046055d8b63161d49495ef428de8f0f2b287b96a
astromuffin22 <156139316+astromuffin22@users.noreply.github.com>
2024-03-23 11:30:28 -0600

    help

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/api/register', async (req, res) => {
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: 'Logged in!' });
+      return res.status(400).json({ message: 'User already exists!' });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -72,27 +72,25 @@ app.post('/api/login', async (req, res) => {
       res.status(500).json({ message: 'Internal server error' });
     }
   });
-  
-  
+
 app.post('/api/addScore', authenticateToken, (req, res) => {
-    const { playerName, pet, chance } = req.body;
-    const newScore = { playerName, pet, chance };
-    scoresData.push(newScore);
+    const data = req.body;
+    scoresData.push(data);
     res.json({ message: 'Score added successfully!', scores: scoresData });
 });
 
 function authenticateToken(req, res, next) {
   const token = req.cookies.token;
-
   if (!token) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
     if (err) {
+      console.error('Token verification error:', err);
       return res.status(403).json({ message: 'Forbidden' });
     }
-    req.user = user;
+    req.decodedToken = decodedToken;
     next();
   });
 }
