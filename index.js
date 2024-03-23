97de6150729b6436c0620f83730e42056683a021
astromuffin22 <156139316+astromuffin22@users.noreply.github.com>
2024-03-22 21:56:11 -0600

    wondering code

--- index.js
+++ index.js
@@ -75,21 +75,6 @@ app.post('/api/addScore', (req, res) => {
   res.json({ message: 'Score added successfully!' });
 });
 
-function authenticateToken(req, res, next) {
-  const token = req.cookies.token;
-
-  if (!token) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      return res.status(403).json({ message: 'Forbidden' });
-    }
-    req.user = user;
-    next();
-  });
-}
 
 app.get('/api/userData', authenticateToken, (req, res) => {
   res.json({ message: 'User data retrieved successfully!' });
