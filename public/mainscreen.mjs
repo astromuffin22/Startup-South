d95cb1d4fbabfbe78400ae7600351a39836f4ced
astromuffin22 <156139316+astromuffin22@users.noreply.github.com>
2024-03-29 21:31:49 -0600

    fix

--- public/mainscreen.mjs
+++ public/mainscreen.mjs
@@ -1,4 +1,4 @@
-import { WebSocket as WebSocket } from 'ws';
+import WebSocket from 'ws';
 
 document.addEventListener('DOMContentLoaded', function () {
     const storedToken = localStorage.getItem('token');
