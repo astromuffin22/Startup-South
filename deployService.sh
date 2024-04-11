e8a146a59b89680d338734bc5b5d4a656e4fbea9
astromuffin22 <156139316+astromuffin22@users.noreply.github.com>
2024-04-10 19:24:19 -0600

    Starting to add react components

--- deployService.sh
+++ deployService.sh
@@ -39,7 +39,7 @@ printf "\n----> Deploy the service on the target\n"
 ssh -i "$key" ubuntu@$hostname << ENDSSH
 bash -i
 cd services/${service}
-npm install express body-parser mongoose mongodb cookie-parser bcrypt jsonwebtoken dotenv ws uuid
+npm install express body-parser mongoose mongodb cookie-parser bcrypt jsonwebtoken dotenv ws uuid react react-dom react-router-dom
 pm2 restart ${service}
 ENDSSH
 
